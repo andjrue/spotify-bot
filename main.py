75cc17f0759f6092401b578778afe26f095f4db4
andjrue <drewsimcox12@gmail.com>
2023-08-11 22:07:01 -0400

    Bot init. Lotta work to do.

---
+++ main.py
@@ -0,0 +1,91 @@
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post, get
+import json
+import discord
+from discord.ext import commands
+
+load_dotenv()
+
+
+"""
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+*                                                                   *
+*                           *** TO-DO ***                           *
+*                                                                   *
+*    - Add Discord features                                         *
+*    - Allow users to pick an artist and play their top n songs     *
+*    - Pick specific songs based on title                           *
+*        - Return top 5 results from song name search               *
+*            - Allow them to respond with 1-5 to pick the songs     *
+*           - The menu will show song name & artist                 *
+*    - Maybe play a playlist so they don't have to chose songs      *
+*      when one ends?                                               *
+*                                                                   *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+"""
+
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+bot_token = os.getenv("BOT_TOKEN")
+
+# print(client_id, client_secret) - DEBUG
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Bot Development
+
+
+def getToken():
+    authString = client_id + ":" + client_secret
+    authBytes = authString.encode("utf-8")
+    authBase64 = str(base64.b64encode(authBytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + authBase64,
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    jsonResult = json.loads(result.content)
+    token = jsonResult["access_token"]
+    return token
+
+
+def getAuthHeader(token):
+    return {"Authorization": "Bearer " + token}
+
+
+def searchForArtist(token, artistName):
+    url = "https://api.spotify.com/v1/search"
+    headers = getAuthHeader(token)
+    query = f"?q={artistName}&type=artist&limit=1"
+
+    queryURL = url + query
+    result = get(queryURL, headers=headers)
+
+    jsonResult = json.loads(result.content)["artists"]["items"]
+
+    if len(jsonResult) == 0:
+        print("Artist not found!")
+        return None
+
+    return jsonResult[0]
+
+
+token = getToken()
+result = searchForArtist(token, "Drake")
+artistID = result["id"]
+# print(token) - DEBUG
+print(result["name"])
+
+
+## RUNS BOT DO NOT DELETE
+bot.run(bot_token)
